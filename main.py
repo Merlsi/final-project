8931f30748268c7940491a578ab94054937c5b38
Merlsi <102343305+Merlsi@users.noreply.github.com>
2022-05-27 21:07:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from ctypes import FormatError
 from tkinter import *
-FormatError tkinter from tkinter import filedialog
+from tkinter import filedialog
+from gtts import gTTS
+import os
 
+window = Tk()
+window.geometry('400x400')
 
 def openFile():
-    filepath = filedialog.askopenfilename(initialdir="/gui/docs",title="SElect a file",filetypes=(("text files","*.txt"),("all files", "*.*")))
+    global file
+    filepath = filedialog.askopenfilename(initialdir='C:\\Users\\1\\Desktop\\project,title="Open file",filetypes',title="Open file",filetype=(("text files",".txt"),("pdf files",".pdf")))
     file = open(filepath, 'r')
-    print(file.read())
-    file.close()
-root = Tk() 
-button = Button(text="Open", command=openFile)
+
+def texttospeech():
+    audio = file.read().replace("\n"," ")
+    language = "en"
+    output = gTTS(text=audio,lang=language,slow=False)
+    output.save("output.mp3")
+    os.system("start output.mp3")
+    
+
+
+
+
+
+button_to_audio = Button(text="click to listen the text",command=texttospeech)
+button_to_audio.place(relx=0.5, rely=0.5,anchor=CENTER, width=160, height=40)
+
+
+
+button = Button(text = "Open",command=openFile)
 button.pack()
-root.mainloop()
+window.mainloop()
