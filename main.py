a9122ed7a848a9a9261f63a6f3f411a238d91a24
Merlsi <102343305+Merlsi@users.noreply.github.com>
2022-05-28 20:49:35 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,60 +3,48 @@ from tkinter import *
 from tkinter import filedialog
 from gtts import gTTS
 import os
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 from tkinter import messagebox
 
 
-#design of tkinter
 window = Tk()
+window.title('Text_to_speech')
 
-#for background
 C = Canvas(window, bg="blue", height=250, width=300)
-filename = PhotoImage(file = "C:\\Users\\1\\Downloads\\backgroun1.png")
+filename = PhotoImage(file="nature1.png")
 background_label = Label(window, image=filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 C.pack()
 
-
-
-
-
-
-#for icon
- 
-
-
-#window.show()
-
+window.iconbitmap(r'alatoo16.ico')
+Label(window, text="Ala Too International University",
+      font='aharoni 30 bold',
+      fg='black', width='45').pack(side='bottom')
 
 
 def openFile():
     global file
-    filepath = filedialog.askopenfilename(initialdir='C:\\Users\\1\\Desktop\\project,title="Open file",filetypes',title="Open file",filetype=(("text files","*.txt"),("pdf files","*.pdf")))
+    filepath = filedialog.askopenfilename(
+        initialdir = 'C: \\Users\\1\\Desktop\\project',
+        title="Open file",
+        filetype=(
+            ("text files", "*.txt"),))
     if(filepath):
         file = open(filepath, 'r')
-        
-    
-            
+# function to convert from text to audio
 
 
 def texttospeech():
-    audio = file.read().replace("\n"," ")
+    audio = file.read().replace("\n", " ")
     language = "en"
-    output = gTTS(text=audio,lang=language,slow=False)
+    output = gTTS(text=audio, lang=language, slow=False)
     output.save("output.mp3")
     os.system("start output.mp3")
-    
+my_font1 = ('times', 14, 'bold')
+button = Button(text="Open file", font=my_font1, command=openFile)
+button.pack(side=LEFT, anchor=NW)
 
-my_font1 = ('times',14,'bold')
-button = Button(text = "Open file",font=my_font1,command=openFile)
-button.pack(pady=10)
-
-
-button_to_audio = Button(text="listen",font=my_font1,command=texttospeech)
-button_to_audio.pack(pady=19)
-#side=LEFT,anchor=N
-
+button_to_audio = Button(text="listen", font=my_font1, command=texttospeech)
+button_to_audio.pack(side=LEFT, anchor=NW)
 
 window.mainloop()
-
